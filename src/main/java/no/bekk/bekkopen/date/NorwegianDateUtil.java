bf4a0dcfa722830c34b96ea5dd29410cfd0006cd
Stein Inge Morisbak <steinim@gmail.com>
2011-09-24 11:21:40 +0200

    POM cleansing and removal of an unused import

--- src/main/java/no/bekk/bekkopen/date/NorwegianDateUtil.java
+++ src/main/java/no/bekk/bekkopen/date/NorwegianDateUtil.java
@@ -5,7 +5,6 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.Locale;
 import java.util.Map;
 import java.util.Set;
