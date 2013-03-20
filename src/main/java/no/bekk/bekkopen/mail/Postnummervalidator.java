3ee13e73a689e83a29627dbe9d08e2366b000b6e
Øyvind Kvangardsnes <meg@oyvind.org>
2013-03-20 21:04:24 +0100

    La til postnummervalidator

---
+++ src/main/java/no/bekk/bekkopen/mail/Postnummervalidator.java
@@ -0,0 +1,11 @@
+package no.bekk.bekkopen.mail;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: oyvind kvangardsnes
+ * Date: 07.03.13
+ * Time: 21.51
+ * To change this template use File | Settings | File Templates.
+ */
+public class Postnummervalidator {
+}
