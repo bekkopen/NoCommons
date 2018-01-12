0336a0b604820341a4cb9c6c6c0bd84310970b71
Eivind Bergstøl <eivind@bergstol.no>
2018-01-12 10:21:33 +0100

    SimpleDateFormat er ikke thread-safe som gjør at testene oppfører seg litt merkelig noen ganger.
    Tests run: 9, Failures: 0, Errors: 1, Skipped: 0, Time elapsed: 0.103 sec <<< FAILURE! - in no.bekk.bekkopen.date.NorwegianDateUtilTest
    testAdd5DaysBeforeNationalDay(no.bekk.bekkopen.date.NorwegianDateUtilTest)  Time elapsed: 0.067 sec  <<< ERROR!
    java.lang.NumberFormatException: For input string: ""
    	at java.lang.NumberFormatException.forInputString(NumberFormatException.java:65)
    	at java.lang.Long.parseLong(Long.java:601)
    	at java.lang.Long.parseLong(Long.java:631)
    	at java.text.DigitList.getLong(DigitList.java:195)
    	at java.text.DecimalFormat.parse(DecimalFormat.java:2051)
    	at java.text.SimpleDateFormat.subParse(SimpleDateFormat.java:2162)
    	at java.text.SimpleDateFormat.parse(SimpleDateFormat.java:1514)
    	at java.text.DateFormat.parse(DateFormat.java:364)
    	at no.bekk.bekkopen.date.NorwegianDateUtilTest.testAdd5DaysBeforeNationalDay(NorwegianDateUtilTest.java:59)

--- src/test/java/no/bekk/bekkopen/date/NorwegianDateUtilTest.java
+++ src/test/java/no/bekk/bekkopen/date/NorwegianDateUtilTest.java
@@ -13,7 +13,9 @@ import java.util.Locale;
 import static org.junit.Assert.*;
 
 public class NorwegianDateUtilTest {
-	private final static DateFormat FORMAT = new SimpleDateFormat("dd.MM.yyyy");
+	private static DateFormat FORMAT(){
+	    return new SimpleDateFormat("dd.MM.yyyy");
+    }
 
 	@Before
 	public void setLocale() {
@@ -23,7 +25,7 @@ public class NorwegianDateUtilTest {
 	@Test
 	public void testAdd2DaysWithinSameWeek() throws Exception {
 		Calendar cal = Calendar.getInstance();
-		cal.setTime(NorwegianDateUtil.addWorkingDaysToDate(FORMAT.parse("21.03.2007"), 2));
+		cal.setTime(NorwegianDateUtil.addWorkingDaysToDate(FORMAT().parse("21.03.2007"), 2));
 
 		assertEquals(23, cal.get(Calendar.DATE));
 	}
@@ -31,7 +33,7 @@ public class NorwegianDateUtilTest {
 	@Test
 	public void testAdd2DaysToLastDayOfMonth() throws Exception {
 		Calendar cal = Calendar.getInstance();
-		cal.setTime(NorwegianDateUtil.addWorkingDaysToDate(FORMAT.parse("28.02.2007"), 2));
+		cal.setTime(NorwegianDateUtil.addWorkingDaysToDate(FORMAT().parse("28.02.2007"), 2));
 
 		assertEquals(2, cal.get(Calendar.DATE));
 		assertEquals(Calendar.MARCH, cal.get(Calendar.MONTH));
@@ -40,7 +42,7 @@ public class NorwegianDateUtilTest {
 	@Test
 	public void testAdd5DaysWithNoHolidays() throws Exception {
 		Calendar cal = Calendar.getInstance();
-		cal.setTime(NorwegianDateUtil.addWorkingDaysToDate(FORMAT.parse("21.03.2007"), 5));
+		cal.setTime(NorwegianDateUtil.addWorkingDaysToDate(FORMAT().parse("21.03.2007"), 5));
 
 		assertEquals(28, cal.get(Calendar.DATE));
 	}
@@ -48,7 +50,7 @@ public class NorwegianDateUtilTest {
 	@Test
 	public void testAdd5DaysBeforeEasterHoliday() throws Exception {
 		Calendar cal = Calendar.getInstance();
-		cal.setTime(NorwegianDateUtil.addWorkingDaysToDate(FORMAT.parse("04.04.2007"), 5));
+		cal.setTime(NorwegianDateUtil.addWorkingDaysToDate(FORMAT().parse("04.04.2007"), 5));
 
 		assertEquals(16, cal.get(Calendar.DATE));
 	}
@@ -56,7 +58,7 @@ public class NorwegianDateUtilTest {
 	@Test
 	public void testAdd5DaysBeforeNationalDay() throws Exception {
 		Calendar cal = Calendar.getInstance();
-		cal.setTime(NorwegianDateUtil.addWorkingDaysToDate(FORMAT.parse("16.05.2007"), 5));
+		cal.setTime(NorwegianDateUtil.addWorkingDaysToDate(FORMAT().parse("16.05.2007"), 5));
 
 		assertEquals(24, cal.get(Calendar.DATE));
 	}
@@ -64,7 +66,7 @@ public class NorwegianDateUtilTest {
 	@Test
 	public void testAdd5DaysBeforeChristmas() throws Exception {
 		Calendar cal = Calendar.getInstance();
-		cal.setTime(NorwegianDateUtil.addWorkingDaysToDate(FORMAT.parse("21.12.2007"), 5));
+		cal.setTime(NorwegianDateUtil.addWorkingDaysToDate(FORMAT().parse("21.12.2007"), 5));
 
 		assertEquals(2, cal.get(Calendar.DATE));
 		assertEquals(Calendar.JANUARY, cal.get(Calendar.MONTH));
@@ -73,10 +75,10 @@ public class NorwegianDateUtilTest {
 
 	@Test
 	public void testWorkingDays() throws Exception {
-		assertFalse("Sunday not working day", NorwegianDateUtil.isWorkingDay(FORMAT.parse("25.03.2007")));
-		assertTrue("Monday is working day", NorwegianDateUtil.isWorkingDay(FORMAT.parse("26.03.2007")));
-		assertFalse("New years day not working day", NorwegianDateUtil.isWorkingDay(FORMAT.parse("01.01.2007")));
-		assertFalse("Easter day not working day", NorwegianDateUtil.isWorkingDay(FORMAT.parse("08.04.2007")));
+		assertFalse("Sunday not working day", NorwegianDateUtil.isWorkingDay(FORMAT().parse("25.03.2007")));
+		assertTrue("Monday is working day", NorwegianDateUtil.isWorkingDay(FORMAT().parse("26.03.2007")));
+		assertFalse("New years day not working day", NorwegianDateUtil.isWorkingDay(FORMAT().parse("01.01.2007")));
+		assertFalse("Easter day not working day", NorwegianDateUtil.isWorkingDay(FORMAT().parse("08.04.2007")));
 	}
 
 	@Test
@@ -113,21 +115,21 @@ public class NorwegianDateUtilTest {
 	public void testGetAllNorwegianHolidaysForYear() {
 		Date[] holidays = NorwegianDateUtil.getHolidays(2008);
 		assertEquals(12, holidays.length);
-		assertEquals("01.01.2008", FORMAT.format(holidays[0]));
-		assertEquals("16.03.2008", FORMAT.format(holidays[1]));
-		assertEquals("20.03.2008", FORMAT.format(holidays[2]));
-		assertEquals("21.03.2008", FORMAT.format(holidays[3]));
-		assertEquals("23.03.2008", FORMAT.format(holidays[4]));
-		assertEquals("24.03.2008", FORMAT.format(holidays[5]));
-		assertEquals("01.05.2008", FORMAT.format(holidays[6]));
-		assertEquals("11.05.2008", FORMAT.format(holidays[7]));
-		assertEquals("12.05.2008", FORMAT.format(holidays[8]));
-		assertEquals("17.05.2008", FORMAT.format(holidays[9]));
-		assertEquals("25.12.2008", FORMAT.format(holidays[10]));
-		assertEquals("26.12.2008", FORMAT.format(holidays[11]));
+		assertEquals("01.01.2008", FORMAT().format(holidays[0]));
+		assertEquals("16.03.2008", FORMAT().format(holidays[1]));
+		assertEquals("20.03.2008", FORMAT().format(holidays[2]));
+		assertEquals("21.03.2008", FORMAT().format(holidays[3]));
+		assertEquals("23.03.2008", FORMAT().format(holidays[4]));
+		assertEquals("24.03.2008", FORMAT().format(holidays[5]));
+		assertEquals("01.05.2008", FORMAT().format(holidays[6]));
+		assertEquals("11.05.2008", FORMAT().format(holidays[7]));
+		assertEquals("12.05.2008", FORMAT().format(holidays[8]));
+		assertEquals("17.05.2008", FORMAT().format(holidays[9]));
+		assertEquals("25.12.2008", FORMAT().format(holidays[10]));
+		assertEquals("26.12.2008", FORMAT().format(holidays[11]));
 	}
 
 	private void checkHoliday(String date) throws ParseException {
-		assertTrue(date, NorwegianDateUtil.isHoliday(FORMAT.parse(date)));
+		assertTrue(date, NorwegianDateUtil.isHoliday(FORMAT().parse(date)));
 	}
 }
