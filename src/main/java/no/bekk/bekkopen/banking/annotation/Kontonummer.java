a776e93e1f02f665bdafc3a127f4924907866a0c
Rune Flobakk <flobakk@gmail.com>
2020-11-26 23:45:45 +0100

    Add missing targets for JSR-303 annotations
    Fixes #40

--- src/main/java/no/bekk/bekkopen/banking/annotation/Kontonummer.java
+++ src/main/java/no/bekk/bekkopen/banking/annotation/Kontonummer.java
@@ -4,15 +4,18 @@ import no.bekk.bekkopen.banking.KontonummerValidator;
 
 import javax.validation.Constraint;
 import javax.validation.Payload;
+
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
 import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
 import static java.lang.annotation.ElementType.FIELD;
 import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.TYPE_PARAMETER;
+import static java.lang.annotation.ElementType.TYPE_USE;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
-@Target({METHOD, FIELD, ANNOTATION_TYPE})
+@Target({METHOD, FIELD, ANNOTATION_TYPE, TYPE_USE, TYPE_PARAMETER})
 @Retention(RUNTIME)
 @Constraint(validatedBy = KontonummerValidator.class)
 public @interface Kontonummer {
--- src/main/java/no/bekk/bekkopen/org/annotation/Organisasjonsnummer.java
+++ src/main/java/no/bekk/bekkopen/org/annotation/Organisasjonsnummer.java
@@ -4,15 +4,18 @@ import no.bekk.bekkopen.org.OrganisasjonsnummerValidator;
 
 import javax.validation.Constraint;
 import javax.validation.Payload;
+
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
 import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
 import static java.lang.annotation.ElementType.FIELD;
 import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.TYPE_PARAMETER;
+import static java.lang.annotation.ElementType.TYPE_USE;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
-@Target( { METHOD, FIELD, ANNOTATION_TYPE })
+@Target({METHOD, FIELD, ANNOTATION_TYPE, TYPE_USE, TYPE_PARAMETER})
 @Retention(RUNTIME)
 @Constraint(validatedBy = OrganisasjonsnummerValidator.class)
 public @interface Organisasjonsnummer {
