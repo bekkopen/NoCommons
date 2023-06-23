4aac04b53bf33d6a74ba914d5ed8592cbd5efcf6
André <andre@metzum.no>
2023-06-23 03:57:18 +0200

    update NorwegianDateUtil: - java8 - make isWorkingDay and isHoliday public - addWorkingDaysToDate allows negative days

--- src/main/java/no/bekk/bekkopen/date/NorwegianDateUtil.java
+++ src/main/java/no/bekk/bekkopen/date/NorwegianDateUtil.java
@@ -26,74 +26,51 @@ package no.bekk.bekkopen.date;
  * #L%
  */
 
-import java.util.Arrays;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.*;
 
 /**
  * Utility class for Norwegian dates.
  */
 public class NorwegianDateUtil {
-	private static Map<Integer, Set<Date>> holidays;
+	private static Map<Integer, Set<LocalDate>> holidays;
 
 	/**
 	 * Adds the given number of working days to the given date. A working day is
 	 * specified as a regular Norwegian working day, excluding weekends and all
 	 * national holidays.
-	 *
+	 * <p>
 	 * Example 1:
 	 * - Add 5 working days to Wednesday 21.03.2007  Yields Wednesday
 	 * 28.03.2007. (skipping saturday and sunday)
-	 *
+	 * <p>
 	 * Example 2:
 	 * - Add 5 working days to Wednesday 04.04.2007 (day before
 	 * easter-long-weekend)  yields Monday 16.04.2007 (skipping 2 weekends and
 	 * 3 weekday holidays).
 	 *
 	 * @param date
-	 *            The original date.
+	 *            The LocalDate object representing the original date.
 	 * @param days
-	 *            The number of working days to add.
+	 *            The number of working days to add, may be negative
 	 * @return The new date.
 	 */
-	public static Date addWorkingDaysToDate(Date date, int days) {
-		Calendar cal = dateToCalendar(date);
+	public static LocalDate addWorkingDaysToDate(LocalDate date, int days) {
 
-		for (int i = 0; i < days; i++) {
-			cal.add(Calendar.DATE, 1);
-			while (!isWorkingDay(cal)) {
-				cal.add(Calendar.DATE, 1);
+		LocalDate curr = LocalDate.from(date);
+		int remainingDays = Math.abs(days);
+		int increment = days >= 0 ? 1 : -1;
+		while (remainingDays > 0) {
+			curr = curr.plusDays(increment);
+			while (!isWorkingDay(curr)) {
+				curr = curr.plusDays(increment);
 			}
+			--remainingDays;
 		}
 
-		return cal.getTime();
-	}
-
-	/**
-	 * Will check if the given date is a working day. That is check if the given
-	 * date is a weekend day or a national holiday.
-	 *
-	 * @param date
-	 *            The date to check.
-	 * @return true if the given date is a working day, false otherwise.
-	 */
-	public static boolean isWorkingDay(Date date) {
-		return isWorkingDay(dateToCalendar(date));
-	}
-
-	/**
-	 * Check if given Date object is a holiday.
-	 *
-	 * @param date
-	 *            The Date to check.
-	 * @return true if holiday, false otherwise.
-	 */
-	public static boolean isHoliday(Date date) {
-		return isHoliday(dateToCalendar(date));
+		return curr;
 	}
 
 	/**
@@ -103,11 +80,42 @@ public class NorwegianDateUtil {
 	 *            The year to get holidays for.
 	 * @return The array of holidays, sorted by date.
 	 */
-	public static Date[] getHolidays(int year) {
-		Set<Date> days = getHolidaySet(year);
-		Date[] dates = days.toArray(new Date[days.size()]);
+	public static LocalDate[] getHolidays(int year) {
+		Set<LocalDate> days = getHolidaySet(year);
+		LocalDate[] dates = days.toArray(new LocalDate[0]);
 		Arrays.sort(dates);
 		return dates;
+	}
+
+	/**
+	 * Will check if the given date is a working day. That is, check if the given
+	 * date is a weekend day or a national holiday.
+	 *
+	 * @param localDate
+	 *            The LocalDate object representing the date.
+	 * @return true if the given date is a working day, false otherwise.
+	 */
+	public static boolean isWorkingDay(LocalDate localDate) {
+		return localDate.getDayOfWeek() != DayOfWeek.SATURDAY && localDate.getDayOfWeek() != DayOfWeek.SUNDAY
+			&& !isHoliday(localDate);
+	}
+
+	/**
+	 * Check if given LocalDate object represents a holiday.
+	 *
+	 * @param localDate
+	 *            The LocalDate to check.
+	 * @return true if localDate is a holiday, false otherwise.
+	 */
+	public static boolean isHoliday(LocalDate localDate) {
+		int year = localDate.getYear();
+		Set<LocalDate> holidays = getHolidaySet(year);
+		for (LocalDate holiday : holidays) {
+			if (localDate.isEqual(holiday)) {
+				return true;
+			}
+		}
+		return false;
 	}
 
 	/**
@@ -117,82 +125,50 @@ public class NorwegianDateUtil {
 	 *            The year to get holidays for.
 	 * @return The set of dates.
 	 */
-	private static Set<Date> getHolidaySet(int year) {
+	private static Set<LocalDate> getHolidaySet(int year) {
 		if (holidays == null) {
 			holidays = new HashMap<>();
 		}
 		if (!holidays.containsKey(year)) {
-			Set<Date> yearSet = new HashSet<>();
+			Set<LocalDate> yearSet = new HashSet<>();
 
 			// Add set holidays.
-			yearSet.add(getDate(1, Calendar.JANUARY, year));
-			yearSet.add(getDate(1, Calendar.MAY, year));
-			yearSet.add(getDate(17, Calendar.MAY, year));
-			yearSet.add(getDate(25, Calendar.DECEMBER, year));
-			yearSet.add(getDate(26, Calendar.DECEMBER, year));
+			yearSet.add(LocalDate.of(year, Month.JANUARY, 1));
+			yearSet.add(LocalDate.of(year, Month.MAY, 1));
+			yearSet.add(LocalDate.of(year, Month.MAY, 17));
+			yearSet.add(LocalDate.of(year, Month.DECEMBER, 25));
+			yearSet.add(LocalDate.of(year, Month.DECEMBER, 26));
 
 			// Add movable holidays - based on easter day.
-			Calendar easterDay = dateToCalendar(getEasterDay(year));
+			LocalDate easterDay = getEasterDay(year);
 
 			// Sunday before easter.
-			yearSet.add(rollGetDate(easterDay, -7));
+			yearSet.add(easterDay.plusDays(-7));
 
 			// Thursday before easter.
-			yearSet.add(rollGetDate(easterDay, -3));
+			yearSet.add(easterDay.plusDays(-3));
 
 			// Friday before easter.
-			yearSet.add(rollGetDate(easterDay, -2));
+			yearSet.add(easterDay.plusDays(-2));
 
 			// Easter day.
-			yearSet.add(easterDay.getTime());
+			yearSet.add(easterDay);
 
 			// Second easter day.
-			yearSet.add(rollGetDate(easterDay, 1));
+			yearSet.add(easterDay.plusDays(1));
 
 			// "Kristi himmelfart" day.
-			yearSet.add(rollGetDate(easterDay, 39));
+			yearSet.add(easterDay.plusDays(39));
 
 			// "Pinse" day.
-			yearSet.add(rollGetDate(easterDay, 49));
+			yearSet.add(easterDay.plusDays(49));
 
 			// Second "Pinse" day.
-			yearSet.add(rollGetDate(easterDay, 50));
+			yearSet.add(easterDay.plusDays(50));
 
 			holidays.put(year, yearSet);
 		}
 		return holidays.get(year);
-	}
-
-	/**
-	 * Will check if the given date is a working day. That is check if the given
-	 * date is a weekend day or a national holiday.
-	 *
-	 * @param cal
-	 *            The Calendar object representing the date.
-	 * @return true if the given date is a working day, false otherwise.
-	 */
-	private static boolean isWorkingDay(Calendar cal) {
-		return cal.get(Calendar.DAY_OF_WEEK) != Calendar.SATURDAY && cal.get(Calendar.DAY_OF_WEEK) != Calendar.SUNDAY
-				&& !isHoliday(cal);
-	}
-
-	/**
-	 * Check if given Calendar object represents a holiday.
-	 *
-	 * @param cal
-	 *            The Calendar to check.
-	 * @return true if holiday, false otherwise.
-	 */
-	private static boolean isHoliday(Calendar cal) {
-		int year = cal.get(Calendar.YEAR);
-		Set<?> yearSet = getHolidaySet(year);
-		for (Object aYearSet : yearSet) {
-			Date date = (Date) aYearSet;
-			if (checkDate(cal, dateToCalendar(date))) {
-				return true;
-			}
-		}
-		return false;
 	}
 
 	/**
@@ -202,9 +178,9 @@ public class NorwegianDateUtil {
 	 *
 	 * @param year
 	 *            The year to calculate from.
-	 * @return The Calendar object representing easter day for the given year.
+	 * @return The LocalDate object representing easter day for the given year.
 	 */
-	private static Date getEasterDay(int year) {
+	private static LocalDate getEasterDay(int year) {
 		int a = year % 19;
 		int b = year / 100;
 		int c = year % 100;
@@ -220,86 +196,7 @@ public class NorwegianDateUtil {
 		int n = (h + l - (7 * m) + 114) / 31; // This is the month number.
 		int p = (h + l - (7 * m) + 114) % 31; // This is the date minus one.
 
-		Calendar cal = Calendar.getInstance();
-		cal.set(Calendar.YEAR, year);
-		cal.set(Calendar.MONTH, n - 1);
-		cal.set(Calendar.DATE, p + 1);
-
-		return cal.getTime();
+		return LocalDate.of(year, n, p + 1);
 	}
 
-	/**
-	 * Check if the given dates match on day and month.
-	 *
-	 * @param cal
-	 *            The Calendar representing the first date.
-	 * @param other
-	 *            The Calendar representing the second date.
-	 * @return true if they match, false otherwise.
-	 */
-	private static boolean checkDate(Calendar cal, Calendar other) {
-		return checkDate(cal, other.get(Calendar.DATE), other.get(Calendar.MONTH));
-	}
-
-	/**
-	 * Check if the given date represents the given date and month.
-	 *
-	 * @param cal
-	 *            The Calendar object representing date to check.
-	 * @param date
-	 *            The date.
-	 * @param month
-	 *            The month.
-	 * @return true if they match, false otherwise.
-	 */
-	private static boolean checkDate(Calendar cal, int date, int month) {
-		return cal.get(Calendar.DATE) == date && cal.get(Calendar.MONTH) == month;
-	}
-
-	/**
-	 * Convert the given Date object to a Calendar instance.
-	 *
-	 * @param date
-	 *            The Date object.
-	 * @return The Calendar instance.
-	 */
-	private static Calendar dateToCalendar(Date date) {
-		Calendar cal = Calendar.getInstance();
-		cal.setTime(date);
-		return cal;
-	}
-
-	/**
-	 * Add the given number of days to the calendar and convert to Date.
-	 *
-	 * @param calendar
-	 *            The calendar to add to.
-	 * @param days
-	 *            The number of days to add.
-	 * @return The date object given by the modified calendar.
-	 */
-	private static Date rollGetDate(Calendar calendar, int days) {
-		Calendar easterSunday = (Calendar) calendar.clone();
-		easterSunday.add(Calendar.DATE, days);
-		return easterSunday.getTime();
-	}
-
-	/**
-	 * Get the date for the given values.
-	 *
-	 * @param day
-	 *            The day.
-	 * @param month
-	 *            The month.
-	 * @param year
-	 *            The year.
-	 * @return The date represented by the given values.
-	 */
-	private static Date getDate(int day, int month, int year) {
-		Calendar cal = Calendar.getInstance();
-		cal.set(Calendar.YEAR, year);
-		cal.set(Calendar.MONTH, month);
-		cal.set(Calendar.DATE, day);
-		return cal.getTime();
-	}
 }
--- src/test/java/no/bekk/bekkopen/date/NorwegianDateUtilTest.java
+++ src/test/java/no/bekk/bekkopen/date/NorwegianDateUtilTest.java
@@ -29,21 +29,17 @@ package no.bekk.bekkopen.date;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class NorwegianDateUtilTest {
-	private static DateFormat FORMAT(){
-	    return new SimpleDateFormat("dd.MM.yyyy");
-    }
+	private static DateTimeFormatter FORMAT(){
+		return DateTimeFormatter.ofPattern("dd.MM.yyyy");
+	}
 
 	@BeforeEach
 	public void setLocale() {
@@ -51,66 +47,105 @@ public class NorwegianDateUtilTest {
 	}
 
 	@Test
-	public void testAdd2DaysWithinSameWeek() throws Exception {
-		Calendar cal = Calendar.getInstance();
-		cal.setTime(NorwegianDateUtil.addWorkingDaysToDate(FORMAT().parse("21.03.2007"), 2));
+	public void testAdd2DaysWithinSameWeek() {
+		LocalDate localDate = NorwegianDateUtil.addWorkingDaysToDate(FORMAT().parse("21.03.2007", LocalDate::from), 2);
 
-		assertEquals(23, cal.get(Calendar.DATE));
+		assertEquals(23, localDate.getDayOfMonth());
 	}
 
 	@Test
-	public void testAdd2DaysToLastDayOfMonth() throws Exception {
-		Calendar cal = Calendar.getInstance();
-		cal.setTime(NorwegianDateUtil.addWorkingDaysToDate(FORMAT().parse("28.02.2007"), 2));
+	public void testAdd2DaysWithinSameWeekBackInTime() {
+		LocalDate localDate = NorwegianDateUtil.addWorkingDaysToDate(FORMAT().parse("23.03.2007", LocalDate::from), -2);
 
-		assertEquals(2, cal.get(Calendar.DATE));
-		assertEquals(Calendar.MARCH, cal.get(Calendar.MONTH));
+		assertEquals(21, localDate.getDayOfMonth());
 	}
 
 	@Test
-	public void testAdd5DaysWithNoHolidays() throws Exception {
-		Calendar cal = Calendar.getInstance();
-		cal.setTime(NorwegianDateUtil.addWorkingDaysToDate(FORMAT().parse("21.03.2007"), 5));
+	public void testAdd2DaysToLastDayOfMonth() {
+		LocalDate localDate = NorwegianDateUtil.addWorkingDaysToDate(FORMAT().parse("28.02.2007", LocalDate::from), 2);
 
-		assertEquals(28, cal.get(Calendar.DATE));
+		assertEquals(2, localDate.getDayOfMonth());
+		assertEquals(Month.MARCH, localDate.getMonth());
 	}
 
 	@Test
-	public void testAdd5DaysBeforeEasterHoliday() throws Exception {
-		Calendar cal = Calendar.getInstance();
-		cal.setTime(NorwegianDateUtil.addWorkingDaysToDate(FORMAT().parse("04.04.2007"), 5));
+	public void testAdd2DaysToLastDayOfMonthBackInTime() {
+		LocalDate localDate = NorwegianDateUtil.addWorkingDaysToDate(FORMAT().parse("02.03.2007", LocalDate::from), -2);
 
-		assertEquals(16, cal.get(Calendar.DATE));
+		assertEquals(28, localDate.getDayOfMonth());
+		assertEquals(Month.FEBRUARY, localDate.getMonth());
 	}
 
 	@Test
-	public void testAdd5DaysBeforeNationalDay() throws Exception {
-		Calendar cal = Calendar.getInstance();
-		cal.setTime(NorwegianDateUtil.addWorkingDaysToDate(FORMAT().parse("16.05.2007"), 5));
+	public void testAdd5DaysWithNoHolidays() {
+		LocalDate localDate = NorwegianDateUtil.addWorkingDaysToDate(FORMAT().parse("21.03.2007", LocalDate::from), 5);
 
-		assertEquals(24, cal.get(Calendar.DATE));
+		assertEquals(28, localDate.getDayOfMonth());
 	}
 
 	@Test
-	public void testAdd5DaysBeforeChristmas() throws Exception {
-		Calendar cal = Calendar.getInstance();
-		cal.setTime(NorwegianDateUtil.addWorkingDaysToDate(FORMAT().parse("21.12.2007"), 5));
+	public void testAdd5DaysWithNoHolidaysBackInTime() {
+		LocalDate localDate = NorwegianDateUtil.addWorkingDaysToDate(FORMAT().parse("28.03.2007", LocalDate::from), -5);
 
-		assertEquals(2, cal.get(Calendar.DATE));
-		assertEquals(Calendar.JANUARY, cal.get(Calendar.MONTH));
-		assertEquals(2008, cal.get(Calendar.YEAR));
+		assertEquals(21, localDate.getDayOfMonth());
 	}
 
 	@Test
-	public void testWorkingDays() throws Exception {
-		assertFalse(NorwegianDateUtil.isWorkingDay(FORMAT().parse("25.03.2007")), "Sunday not working day");
-		assertTrue(NorwegianDateUtil.isWorkingDay(FORMAT().parse("26.03.2007")), "Monday is working day");
-		assertFalse(NorwegianDateUtil.isWorkingDay(FORMAT().parse("01.01.2007")), "New years day not working day");
-		assertFalse(NorwegianDateUtil.isWorkingDay(FORMAT().parse("08.04.2007")), "Easter day not working day");
+	public void testAdd5DaysBeforeEasterHoliday() {
+		LocalDate localDate = NorwegianDateUtil.addWorkingDaysToDate(FORMAT().parse("04.04.2007", LocalDate::from), 5);
+
+		assertEquals(16, localDate.getDayOfMonth());
 	}
 
 	@Test
-	public void testVariousNorwegianHolidays() throws Exception {
+	public void testAdd5DaysBeforeEasterHolidayBackInTime() {
+		LocalDate localDate = NorwegianDateUtil.addWorkingDaysToDate(FORMAT().parse("16.04.2007", LocalDate::from), -5);
+
+		assertEquals(4, localDate.getDayOfMonth());
+	}
+
+	@Test
+	public void testAdd5DaysBeforeNationalDay() {
+		LocalDate localDate = NorwegianDateUtil.addWorkingDaysToDate(FORMAT().parse("16.05.2007", LocalDate::from), 5);
+
+		assertEquals(24, localDate.getDayOfMonth());
+	}
+
+	@Test
+	public void testAdd5DaysBeforeNationalDayBackInTime() {
+		LocalDate localDate = NorwegianDateUtil.addWorkingDaysToDate(FORMAT().parse("24.05.2007", LocalDate::from), -5);
+
+		assertEquals(16, localDate.getDayOfMonth());
+	}
+
+	@Test
+	public void testAdd5DaysBeforeChristmas() {
+		LocalDate localDate = NorwegianDateUtil.addWorkingDaysToDate(FORMAT().parse("21.12.2007", LocalDate::from), 5);
+
+		assertEquals(2, localDate.getDayOfMonth());
+		assertEquals(Month.JANUARY, localDate.getMonth());
+		assertEquals(2008, localDate.getYear());
+	}
+
+	@Test
+	public void testAdd5DaysBeforeChristmasBackInTime() {
+		LocalDate localDate = NorwegianDateUtil.addWorkingDaysToDate(FORMAT().parse("02.01.2008", LocalDate::from), -5);
+
+		assertEquals(21, localDate.getDayOfMonth());
+		assertEquals(Month.DECEMBER, localDate.getMonth());
+		assertEquals(2007, localDate.getYear());
+	}
+
+	@Test
+	public void testWorkingDays() {
+		assertFalse(NorwegianDateUtil.isWorkingDay(FORMAT().parse("25.03.2007", LocalDate::from)), "Sunday not working day");
+		assertTrue(NorwegianDateUtil.isWorkingDay(FORMAT().parse("26.03.2007", LocalDate::from)), "Monday is working day");
+		assertFalse(NorwegianDateUtil.isWorkingDay(FORMAT().parse("01.01.2007", LocalDate::from)), "New years day not working day");
+		assertFalse(NorwegianDateUtil.isWorkingDay(FORMAT().parse("08.04.2007", LocalDate::from)), "Easter day not working day");
+	}
+
+	@Test
+	public void testVariousNorwegianHolidays() {
 		// Set dates
 		checkHoliday("01.01.2007");
 		checkHoliday("01.05.2007");
@@ -141,7 +176,7 @@ public class NorwegianDateUtilTest {
 
 	@Test
 	public void testGetAllNorwegianHolidaysForYear() {
-		Date[] holidays = NorwegianDateUtil.getHolidays(2009);
+		LocalDate[] holidays = NorwegianDateUtil.getHolidays(2009);
 		assertEquals(13, holidays.length);
 		assertEquals("01.01.2009", FORMAT().format(holidays[0]));
 		assertEquals("05.04.2009", FORMAT().format(holidays[1]));
@@ -158,7 +193,7 @@ public class NorwegianDateUtilTest {
 		assertEquals("26.12.2009", FORMAT().format(holidays[12]));
 	}
 
-	private void checkHoliday(String date) throws ParseException {
-		assertTrue(NorwegianDateUtil.isHoliday(FORMAT().parse(date)), date);
+	private void checkHoliday(String date) {
+		assertTrue(NorwegianDateUtil.isHoliday(FORMAT().parse(date, LocalDate::from)), date);
 	}
 }
